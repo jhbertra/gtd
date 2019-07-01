899813cbf0580433c99f296fa9ffe86606ad699c
Jamie Bertram <jamie@novoideation.ca>
2019-07-01 15:32:29 -0400

    Add In command handler

--- src/Gtd/Cli.hs
+++ src/Gtd/Cli.hs
@@ -1,13 +1,23 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+
 module Gtd.Cli
     ( defaultMain
     ) where
 
+import Control.Monad (forM_)
+import Data.List (sort)
+import Data.Text (Text)
+import Database.HDBC (IConnection, commit)
 import System.Console.CmdArgs
 
+import Gtd
+import Gtd.Database
+
+import qualified Data.Text.IO as TIO
+
 data Opts
-    = In { items :: [String] }
-    deriving (Data,Typeable,Show,Eq)
+    = In { items :: [Text] }
+    deriving (Data, Typeable, Show, Eq)
 
 in_ :: Opts
 in_ = In
@@ -21,4 +31,15 @@ defaultMain = do
         &= help "Getting things done"
         &= program "gtd"
         &= summary "Gtd v1.0"
-    print opts
+    c <- connect
+    case opts of
+        In [] -> showInList c
+        In items -> addToInList c items
+
+showInList :: (IConnection c) => c -> IO ()
+showInList c = getInItems c >>= mapM_ (TIO.putStrLn . inItemName) . sort
+
+addToInList :: (IConnection c) => c -> [Text] -> IO ()
+addToInList c items = do
+    forM_ items $ addInItem c . InItem 0
+    commit c
