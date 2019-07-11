04a29ed4603bf6ad7c04cb5a84597ecdb716664e
Jamie Bertram <jamie@novoideation.ca>
2019-07-10 22:06:01 -0400

    Add interactive in list processing

--- src/Gtd/Cli.hs
+++ src/Gtd/Cli.hs
@@ -5,21 +5,27 @@ module Gtd.Cli
     ) where
 
 import Control.Monad (forM_)
+import Control.Monad.IO.Class (liftIO)
+import Control.Monad.State.Lazy (StateT, execStateT, get, put, modify)
 import Data.List (sort)
-import Data.Maybe (isJust)
+import Data.Maybe (fromMaybe, isJust)
 import Data.Text (Text, unpack)
-import Database.HDBC (IConnection, commit)
+import Data.Time.LocalTime
+import Database.HDBC (IConnection, commit, rollback)
 import System.Console.CmdArgs
 import System.IO
+import Text.Read (readMaybe)
 
 import Gtd
 import Gtd.Database
 
+import qualified Data.Char as C
+import qualified Data.Set as S
 import qualified Data.Text.IO as TIO
 import qualified Data.Text as T
 
 data Opts
-    = In { items :: [Text], delete :: Bool, rename :: Bool }
+    = In { items :: [Text], delete :: Bool, rename :: Bool, interactive :: Bool }
     deriving (Data, Typeable, Show, Eq)
 
 in_ :: Opts
@@ -27,6 +33,7 @@ in_ = In
     { items = def &= args
     , delete = def &= help "Delete the items"
     , rename = def &= name "m" &= help "Rename the items"
+    , interactive = def &= name "i" &= help "Interactively process the in list"
     } &= help "Add item(s) to your \"in\" list" &= auto
 
 defaultMain :: IO ()
@@ -38,10 +45,11 @@ defaultMain = do
         &= summary "Gtd v1.0"
     c <- connect
     case opts of
-        In [] _ _ -> showInList c
-        In items True _ -> deleteFromInList c items
-        In items _ True -> renameInInList c items
-        In items _ _ -> addToInList c items
+        In _ _ _ True -> processInList c
+        In [] _ _ _ -> showInList c
+        In items True _ _ -> deleteFromInList c items
+        In items _ True _ -> renameInInList c items
+        In items _ _ _ -> addToInList c items
 
 showInList :: (IConnection c) => c -> IO ()
 showInList c = getInItems c >>= mapM_ (TIO.putStrLn . inItemName) . sort
@@ -83,3 +91,136 @@ renameInInList c items = do
                             then errorWithoutStackTrace "gtd: New name conflicts with existing in list item"
                             else updateInItem c $ loaded' { inItemName = newName }
     commit c
+
+data ProcessInListOption
+    = DoIt
+    | NextAction
+    | Project
+    | Delegate
+    | SomeDay
+    | Incubate
+    | Trash
+    | Quit
+
+data InListProcessState = InListProcessState
+    { inProcessContinue :: !Bool
+    , inProcessInList :: !InList
+    , inProcessNextActions :: !NextActionsList
+    , inProcessWaitingFor :: !WaitingForList
+    }
+    deriving (Show)
+
+processInList :: (IConnection c) => c -> IO ()
+processInList c = do
+    initialState <- loadInitialState
+    if null $ inListToList . inProcessInList $ initialState
+        then putStrLn "\nIn list empty"
+        else do
+            state <- execStateT (mapM_ processInItem $ inListToList . inProcessInList $ initialState) initialState
+            if inProcessContinue state
+                then commitState c initialState state
+                else rollback c
+    putStrLn ""
+  where
+    loadInitialState = InListProcessState
+        True
+        <$> fmap inListfromList (getInItems c)
+        <*> fmap nextActionsfromList (getActions c)
+        <*> fmap waitingForfromList (getDelegatedActions c)
+
+    commitState c initialState state = do
+        forM_ (deletedInItems initialState state) $ deleteInItem c
+        forM_ (addedActions initialState state) $ addAction c
+        forM_ (addedDelegatedActions initialState state) $ addDelegatedAction c
+        commit c
+    
+    deletedInItems initialState state =
+        let (oldSet, newSet) = mapTuple (S.fromList . inListToList . inProcessInList) (initialState, state)
+        in S.toList $ S.difference oldSet newSet
+    
+    addedActions initialState state =
+        let (oldSet, newSet) = mapTuple (S.fromList . nextActionsToList . inProcessNextActions) (initialState, state)
+        in S.toList $ S.difference newSet oldSet
+    
+    addedDelegatedActions initialState state =
+        let (oldSet, newSet) = mapTuple (S.fromList . waitingForToList . inProcessWaitingFor) (initialState, state)
+        in S.toList $ S.difference newSet oldSet
+
+processInItem :: InItem -> StateT InListProcessState IO ()
+processInItem item = do
+    continue <- fmap inProcessContinue get
+    if continue
+        then do
+            choice <- liftIO $ getChoice item
+            handleChoice choice item
+        else
+            pure ()
+  where
+    getChoice item = do
+        putStrLn ""
+        putStrLn $ "How do you want to handle " ++ (show . inItemName) item ++ "?"
+        putStrLn ""
+        putStrLn "    1. Do it now (it will take me < 2 min)."
+        putStrLn "    2. Put it into my next actions list."
+        putStrLn "    3. Make a new project for it."
+        putStrLn "    4. Delegate it."
+        putStrLn "    5. Do it some day / maybe."
+        putStrLn "    6. Sit on it a while longer."
+        putStrLn "    7. Trash it."
+        putStrLn "    8. Quit."
+        putStrLn ""
+        putStr "Selection: "
+        hFlush stdout
+        choice <- parseChoice <$> getLine
+        maybe (putStrLn "" >> putStrLn "That is not a valid option" >> getChoice item) pure choice
+    
+    parseChoice s =
+        case readMaybe s of
+            Just 1 -> Just DoIt
+            Just 2 -> Just NextAction
+            Just 3 -> Just Project
+            Just 4 -> Just Delegate
+            Just 5 -> Just SomeDay
+            Just 6 -> Just Incubate
+            Just 7 -> Just Trash
+            Just 8 -> Just Quit
+            _ -> Nothing
+    
+    handleChoice :: ProcessInListOption -> InItem -> StateT InListProcessState IO ()
+    handleChoice DoIt item = do
+        _ <- liftIO $ do
+            putStrLn ""
+            putStr "Go do it! (press any key to continue)"
+            hFlush stdout
+            getLine
+        modify $ \s -> s { inProcessInList = doInItem (inItemName item) $ inProcessInList s }
+    
+    handleChoice NextAction item = modify $ \s ->
+        let (inList', nextActions') = inItemToNextAction (inItemName item) (inProcessInList s) (inProcessNextActions s)
+        in s { inProcessInList = inList', inProcessNextActions = nextActions' }
+    
+    handleChoice Project item = pure ()
+    
+    handleChoice Delegate item = do
+        (delegate, day) <- liftIO $ (,) <$> getDelegate <*> getDay
+        modify $ \s ->
+            let (inList', waitingFor) = delegateInItem (inItemName item) delegate day (inProcessInList s) (inProcessWaitingFor s)
+            in s { inProcessInList = inList', inProcessWaitingFor = waitingFor }
+    
+    handleChoice SomeDay item = pure ()
+    handleChoice Incubate item = pure ()
+    handleChoice Trash item = modify $ \s -> s { inProcessInList = doInItem (inItemName item) $ inProcessInList s }
+    handleChoice Quit item = modify $ \s -> s { inProcessContinue = False }
+
+    getDelegate = do
+        putStr "Who is this being delegated to? "
+        hFlush stdout
+        delegate <- TIO.getLine
+        if T.null delegate
+            then getDelegate
+            else pure delegate
+
+    getDay = fmap (localDay . zonedTimeToLocalTime) getZonedTime
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (a1, a2) = (f a1, f a2)
